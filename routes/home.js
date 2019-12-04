32057d3b92b23fa6512791c8a52b6ac6e964ac76
Alex Davidoff <ad35@pdx.edu>
2019-12-04 13:49:11 -0800

    create template pug file

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var user = require('./landing.js');
+var user = require('./index');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'User Map' });
-})
-
-
+  res.render('home', { title: 'User Map', username: req.body.username, home: req.body.address });
+  console.log(req.body.username, req.body.address);
+});
 
 module.exports = router;
