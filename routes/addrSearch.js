8bd7ed964a26e6eeca5222bcc8999ccf9c55d3e7
Alex Davidoff <ad35@pdx.edu>
2019-12-04 14:13:30 -0800

    added addrSearch

---
+++ routes/addrSearch.js
@@ -0,0 +1,29 @@
+const search = 'https://maps.googleapis.com/maps/api/place/textsearch/json';
+const apiKey = 'AIzaSyAYNWMjJ6GEX_Ja-l9iWLVFnzh4MCxSvE0';
+const https = require('https');
+let result = [];
+let callBack = function(data) {
+  result.push(...data);
+  return result;
+}
+
+const getData = function (url, callback) {
+  https.get(url, (res) => {
+    let body = '';    
+    res.on('data', function (data) {
+      body += data;
+    });
+
+    res.on('end', function () {
+      if(JSON.parse(body).results !== null){
+        callBack(JSON.parse(body));
+      }
+    });
+  });
+}
+
+module.exports = query => {
+  const { address } = query;
+  let url = `${search}?key=${apiKey}&query=${address}`
+  getData(url, callBack);
+};
