58d3f6176ac19c0a80df7874a82226a0ae2cbc94
Alex Davidoff <ad35@pdx.edu>
2019-12-04 13:52:07 -0800

    merging to master

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var username = "";
+const home = require('./home');
+const isUser = require('./users');
+const addrSearch = require('./addrSearch');
 
 /* GET landing page. */
 router.get('/', function(req, res, next) {
@@ -8,5 +10,19 @@ router.get('/', function(req, res, next) {
 });
 
 
+router.post('/submit', function(req, res, next) {
+  const username = req.body.username; 
+  const address = req.body.address;
+  if(isUser(username)) 
+    res.render('home', { user: username, address: address });
+  else
+    return next();
+}, function(req, res, next, username) { // new user must enter home address
+  var address = req.body.address;
+  var query = address.split(' ').join('+');
+  //var result = addrSearch(query); // implemented in addrSearch.js
+  //console.log(result);
+    res.render('home', { user: username, home: query }); // pass new user and address query
+});
+
 module.exports = router;
-exports.username = username;
