fcb460fb76836eb7438f78c179ae75e1a8aa77bb
alex-bailey1 <alexbailey@pdx.edu>
2019-12-05 11:54:00 -0800

    adding user insert

--- app.js
+++ app.js
@@ -175,6 +175,54 @@ app.post("/test_db_insert", (req, res) => {
   // res.end()
 });
 
+
+app.post('/db_insert_user', (req, res) =>{
+  fs.readFile('./users.json', 'utf8', (err, jsonString) => {
+      if (err) {
+          console.log("File read failed:", err)
+          res.end()
+          return
+      }
+
+      let req_username = req.body.username;
+      let req_home_cord_first = req.body.home_cord_first;
+      let req_home_cord_second = req.body.home_cord_second;
+      
+
+
+      let users_json_all = JSON.parse(jsonString);
+
+      let new_db_item = {
+          "userid": 1,
+          "name": req_username,
+          "home": {
+              "coordinates": [req_home_cord_first, req_home_cord_second]
+          }
+      }
+
+      users_json_all["users"].push(new_db_item);
+
+      // console.log(req_home_cord)
+      // console.log(users_json_all);
+      // console.log(users_json_all["users"]);
+      
+
+      let new_json_string = JSON.stringify(users_json_all);
+      fs.writeFile('./users.json', new_json_string, err => {
+          if(err){
+              console.log(err)
+          }
+          else
+          {
+              // console.log("write successful");
+          }
+      });
+      res.end();
+          
+  })
+  // res.end()
+});
+
 app.listen(8080);
 
 module.exports = app;
